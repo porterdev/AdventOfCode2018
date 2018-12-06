b852242adf795cdfe722cfe88ae19b40ed3cc712
porterdev <>
2018-12-06 16:10:51 +1100

    add day 3 project

---
+++ day3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //load the input
+
+            string readText = File.ReadAllText("input.txt");
+
+            var stringValues = readText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
